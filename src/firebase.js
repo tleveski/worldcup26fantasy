ab63000630f76e323eb19fc1a1aeab74db836b3d
tleveski <Tyler.leveski@gmail.com>
2026-06-13 19:44:26 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, setDoc, onSnapshot, getDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyBjpebarVAoZgYe3J7M5I9nAPS_7QpLTNA",
-  authDomain:        "wc26-fantasy.firebaseapp.com",
-  projectId:         "wc26-fantasy",
-  storageBucket:     "wc26-fantasy.firebasestorage.app",
-  messagingSenderId: "44533981101",
-  appId:             "1:44533981101:web:47e6faf43437a76ea40b24",
+  apiKey:            process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app       = initializeApp(firebaseConfig);
